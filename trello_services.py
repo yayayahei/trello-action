9d799f17b68293cf9439dbc9d7475e6d80c4ba3d
yayayahei <mmwcbz@msn.cn>
2021-02-21 22:42:34 +0800

    fix code style issue

--- trello_services.py
+++ trello_services.py
@@ -9,14 +9,14 @@ client = TrelloClient(
 
 
 def delete_board_archived_items():
-    english_board_id = "yTsv60N4"
-    english_board = client.get_board(english_board_id)
+    board_id = "yTsv60N4"
+    board = client.get_board(board_id)
 
-    print(english_board.name)
-    closed_cards = english_board.closed_cards()
+    print(board.name)
+    closed_cards = board.closed_cards()
     for closed_card in closed_cards:
         print(closed_card.name)
         closed_card.delete()
-    # lists = english_board.list_lists()
+    # lists = board.list_lists()
     # for board_list in lists:
     #     print(board_list.name)
